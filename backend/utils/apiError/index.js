8feabe8b97278b2b5fc7c87aff6aa488baad57ae
squdward <squdward13@gmail.com>
2024-02-15 23:03:49 +0300

    Add utils code

---
+++ backend/utils/apiError/index.js
@@ -0,0 +1,22 @@
+module.exports = class ApiError extends Error {
+    status;
+    errors;
+
+    constructor(status, message, errors = []) {
+        super(message);
+        this.status = status;
+        this.errors = errors;
+    }
+
+    static UnauthorizedError(message) {
+        return new ApiError(401, message)
+    }
+
+    static BadRequest(message, errors = []) {
+        return new ApiError(400, message, errors);
+    }
+    
+    static NotFound(message) {
+        return new ApiError(404, message)
+    }
+}
