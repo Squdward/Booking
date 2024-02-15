cfb85d734480ff8d7565a92a0edc0b22936baff3
squdward <squdward13@gmail.com>
2024-02-16 00:15:12 +0300

    add cookie-parser package

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,33 @@
-require('dotenv').config();
-const cors = require('cors')
-const express = require('express');
-const mongoose = require('mongoose');
-const router = require('./router');
-const errorMiddleware = require('./middlwares/error');
+require("dotenv").config();
+const cors = require("cors");
+const express = require("express");
+const mongoose = require("mongoose");
+const router = require("./router");
+const errorMiddleware = require("./middlwares/error");
+const cookieParser = require("cookie-parser");
 
 const port = process.env.BACKEND_PORT || 9999;
+
 const app = express();
 
-app.use(express.json());
-app.use(cors({
-    origin: `http://localhost:${process.env.FRONTEND_PORT}`
-}))
+mongoose
+    .connect(
+        `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.ash6bi2.mongodb.net/?retryWrites=true&w=majority`
+    )
+    .then(() => console.log("DB OK"))
+    .catch((err) => console.log("DB ERROR", err));
 
-mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.ash6bi2.mongodb.net/?retryWrites=true&w=majority`)
-.then(() => console.log('DB OK'))
-.catch((err) => console.log('DB ERROR', err));
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+    cors({
+        origin: `http://localhost:${process.env.FRONTEND_PORT}`,
+    })
+);
 
 app.listen(port, () => {
-    console.log(`Server start at port ${port}`)
-})
+    console.log(`Server start at port ${port}`);
+});
 
-app.use('/api', router);
+app.use("/api", router);
 app.use(errorMiddleware);
