c9a1cae24c9d23db55b370ce5c4d2b581f36ac14
squdward <squdward13@gmail.com>
2024-02-20 22:23:39 +0300

    Add support static files

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,8 @@ const mongoose = require("mongoose");
 const router = require("./router");
 const errorMiddleware = require("./middlwares/error");
 const cookieParser = require("cookie-parser");
-
+const path = require('path');
+const multer = require("multer");
 const port = process.env.BACKEND_PORT || 9999;
 
 const app = express();
@@ -25,6 +26,28 @@ app.use(
         credentials: true,
     })
 );
+
+// Указываем директорию для сохранения загруженных изображений
+const uploadDirectory = path.join(__dirname, 'uploads');
+
+// Используем Multer для загрузки файлов
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, uploadDirectory);
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + '-' + file.originalname);
+    }
+});
+
+const upload = multer({ storage: storage });
+
+// Маршрут для обслуживания изображений
+app.get('/uploads/:folder/:imageName', (req, res) => {
+    const {imageName, folder} = req.params;
+    const imagePath = path.join(uploadDirectory + `/${folder}`, imageName);
+    res.sendFile(imagePath);
+});
 
 app.listen(port, () => {
     console.log(`Server start at port ${port}`);
