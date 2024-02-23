33dda5e1e9e0efeb308bb37c81d7a084c13f8f1d
squdward <squdward13@gmail.com>
2024-02-23 16:49:40 +0300

    refactor image routes

--- backend/utils/imageRoutes/index.js
+++ backend/utils/imageRoutes/index.js
@@ -1,20 +1,35 @@
 const path = require("path");
 const multer = require("multer");
 
+const uploadDirectory = path.join(__dirname, '../..', "uploads");
+
+// Функция для определения папки назначения для изображений авторов
+const authorImageStorage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, uploadDirectory + "/authors");
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + "-" + file.originalname);
+    }
+});
+
+// Функция для определения папки назначения для обложек книг
+const bookCoverStorage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, uploadDirectory + "/books");
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + "-" + file.originalname);
+    }
+});
+
+// Настройка multer для изображений авторов
+const uploadAuthorImage = multer({ storage: authorImageStorage });
+
+// Настройка multer для обложек книг
+const uploadBookCover = multer({ storage: bookCoverStorage });
+
 function setupImageRoutes(app) {
-    const uploadDirectory = path.join(__dirname, '../..', "uploads");
-    
-    const storage = multer.diskStorage({
-        destination: (req, file, cb) => {
-            cb(null, uploadDirectory);
-        },
-        filename: (req, file, cb) => {
-            cb(null, Date.now() + "-" + file.originalname);
-        },
-    });
-
-    const upload = multer({ storage: storage });
-
     app.get("/uploads/:folder/:imageName", (req, res) => {
         const { imageName, folder } = req.params;
         const imagePath = path.join(uploadDirectory + `/${folder}`, imageName);
@@ -22,4 +37,4 @@ function setupImageRoutes(app) {
     });
 }
 
-module.exports = { setupImageRoutes };
+module.exports = { uploadAuthorImage, uploadBookCover, setupImageRoutes };
