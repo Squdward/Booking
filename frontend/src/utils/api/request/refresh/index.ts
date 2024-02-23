52d524a570b37ef578d7c38f65afa8c770c9d97d
squdward <squdward13@gmail.com>
2024-02-23 11:30:03 +0300

    Add refreshToken route

--- frontend/src/utils/api/request/refresh/index.ts
+++ frontend/src/utils/api/request/refresh/index.ts
@@ -1,4 +1,14 @@
 import axios, { AxiosRequestConfig } from "axios";
 import { BASE_API_NAME } from "../../instanse";
+import { RequestConfig, TypeAccesToken } from "../../../../types/api";
+import { IUser } from "../../../../types/user";
 
-export const refreshToken = async (config: AxiosRequestConfig) => axios.get(`${BASE_API_NAME}/refresh`, config)
+interface RefreshTokenConfig {
+    user: Omit<IUser, "password">
+    accesToken: TypeAccesToken
+}
+
+type RefreshConfig = RequestConfig<RefreshTokenConfig>
+
+
+export const refreshToken = async (config?: AxiosRequestConfig):RefreshConfig => axios.get(`${BASE_API_NAME}/refresh`, config)
