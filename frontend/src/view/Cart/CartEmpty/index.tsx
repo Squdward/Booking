f0a4aa051193cb597e7fda60740894c950ce00fb
squdward <squdward13@gmail.com>
2024-03-25 17:00:29 +0300

    feat/complete empty state for cart

--- frontend/src/view/Cart/CartEmpty/index.tsx
+++ frontend/src/view/Cart/CartEmpty/index.tsx
@@ -1,10 +1,11 @@
 import Icon from "../../../assets/Cart/emptyCart.svg?react";
+import styles from "./index.module.scss";
 
 const CartEmpty = () => {
     return (
-        <div>
-            Nothing found
+        <div className={styles.empty}>
             <Icon/>
+            <h1>Nothing found</h1>
         </div>
     )
 }
